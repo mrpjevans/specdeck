e553588c2453dd46ee4f9a0907edcb6d8e2a7723
PJ Evans <pj@mrpjevans.com>
2022-04-17 12:46:15 +0100

    Change to remove keyboard module

--- specdeck.py
+++ specdeck.py
@@ -1,9 +1,15 @@
 from time import sleep
 import pygame.mixer
-import os, subprocess
-import keyboard
+import os, subprocess, io
 from gpiozero import Device, Button
 from gpiozero.pins.mock import MockFactory
+
+def is_raspberrypi():
+    try:
+        with io.open('/sys/firmware/devicetree/base/model', 'r') as m:
+            if 'raspberry pi' in m.read().lower(): return True
+    except Exception: pass
+    return False
 
 print("SpecDeck!")
 my_directory = os.path.dirname(os.path.realpath(__file__))
@@ -16,7 +22,7 @@ config = {
 
 print("Initialising")
 # Allows development on non-Raspberry Pi platforms
-if Device.pin_factory is None:
+if is_raspberrypi() is False:
     Device.pin_factory = MockFactory()
 button_a = Button(5)
 pygame.mixer.init()
@@ -31,7 +37,7 @@ pygame.mixer.music.play()
 
 # Main event loop
 while True:
-    if button_a.is_pressed or keyboard.read_key() == 'p':
+    if button_a.is_pressed:
         if pygame.mixer.music.get_busy():
             print("Pausing")
             pygame.mixer.music.pause()
