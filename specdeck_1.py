4d4e521c3da732ec52273302c3413e962dc5d163
PJ Evans <pj@mrpjevans.com>
2022-04-28 18:10:57 +0100

    Add Raspberry Pi logo loader

--- specdeck_1.py
+++ specdeck_1.py
@@ -2,7 +2,7 @@ from time import sleep
 import pygame.mixer
 pygame.mixer.init()
 pygame.mixer.music.set_volume(1)
-pygame.mixer.music.load("tapes/Jetpac.wav")
+pygame.mixer.music.load("./raspberrypi.wav")
 pygame.mixer.music.play()
 while True:
     sleep(0.1)
